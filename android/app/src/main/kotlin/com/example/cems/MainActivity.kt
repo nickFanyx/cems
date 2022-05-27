383ddf913202e6de2a4b6c3963194691bc9aa5e7
nickFanyx <farihinzulkifli@gmail.com>
2022-05-27 15:42:43 +0800

    first commit

---
+++ android/app/src/main/kotlin/com/example/cems/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.cems
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Data layer/daily_record.dart
@@ -0,0 +1,4 @@
+class DailyRec {
+  final String name;
+  DailyRec(this.name);
+}
