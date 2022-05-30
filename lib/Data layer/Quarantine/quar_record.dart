6b4d47af6fa9fd8b01eb926b86d77d7f908e176c
nickFanyx <farihinzulkifli@gmail.com>
2022-05-30 16:02:49 +0800

    quarantine

--- lib/Data layer/Quarantine/quar_record.dart
+++ lib/Data layer/Quarantine/quar_record.dart
@@ -1,3 +1,1 @@
-class QuarantineModel{
-  
-}
+class QuarantineModel {}
--- lib/Interface layer/Quarantine/UserView/manage_quar_rec_ui.dart
+++ lib/Interface layer/Quarantine/UserView/manage_quar_rec_ui.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class UManageQuarRec extends StatefulWidget {
@@ -11,8 +10,90 @@ class UManageQuarRec extends StatefulWidget {
 class _UManageQuarRecState extends State<UManageQuarRec> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: const Text('managing'),
+    return Scaffold(
+      body: Column(
+        children: [
+          const SizedBox(
+            height: 20,
+          ),
+          Container(
+            height: 300,
+            color: Colors.redAccent,
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Center(
+            child: InkWell(
+              onTap: () {},
+              child: Card(
+                  elevation: 10,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: SizedBox(
+                    width: 300,
+                    height: 90,
+                    child: Row(
+                      children: [
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Image(
+                          image: AssetImage('assets/images/addquar.png'),
+                          width: 50,
+                          height: 50,
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Text(
+                          'Add Quarantine Record',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                      ],
+                    ),
+                  )),
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Center(
+            child: InkWell(
+              onTap: () {},
+              child: Card(
+                  elevation: 10,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: SizedBox(
+                    width: 300,
+                    height: 90,
+                    child: Row(
+                      children: [
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Image(
+                          image: AssetImage('assets/images/cov.png'),
+                          width: 50,
+                          height: 50,
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Text(
+                          'Covid-19 Daily Record',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                      ],
+                    ),
+                  )),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
